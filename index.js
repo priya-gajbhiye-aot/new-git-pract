8a2bbd897307cfd3ce667532e3ee216191d8d264
priya-gajbhiye-aot <priya.gajbhiye@algoocean.com>
2022-08-10 11:08:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+function add(a , b){
+    const sum=a+b;
+    console.log(sum)
+}
+
+add(5,6);
+
+function sub(a , b){
+    const sub1=a-b;
+    console.log(sub1)
+}
+
+add(9,6);
+
+function mult(a , b){
+    const mul=a*b;
+    console.log(mul)
+}
+
+add(9,6);
