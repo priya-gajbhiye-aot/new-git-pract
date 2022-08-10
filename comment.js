0c2fe2ec0ad6027f4c31be5e1cad9ab57b538bef
priya <priya.gajbhiye@algoocean.com>
2022-08-10 14:59:48 +0530

    this is updated files

---
+++ comment.js
@@ -0,0 +1,13 @@
+function mult(a, b){
+    const mul=a*b;
+    console.log("my first project to do" +mul)
+}
+
+add(9,6);
+
+function search(a, b){
+    const mul1=a*b;
+    console.log("my first project" +mul1)
+}
+
+search(90,60);
